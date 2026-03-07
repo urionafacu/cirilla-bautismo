58bb72dab25fb7314e62d0e10c474b0a8997b949
Facundo Uriona <urionafacu@gmail.com>
2026-03-07 19:04:48 -0300

    improve og

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.SITE_URL ?? 'https://cirilla-bautismo.pages.dev',
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
