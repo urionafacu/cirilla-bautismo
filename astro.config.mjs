c2f270b8c5feb46914d86747b214c5aaf988ae35
Facundo Uriona <urionafacu@gmail.com>
2026-03-07 18:45:26 -0300

    add oc picture

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: process.env.SITE_URL ?? 'https://cirilla-bautismo.pages.dev',
+});
